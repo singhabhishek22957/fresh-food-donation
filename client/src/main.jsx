010b6d4fa5056ad8b2a977ae221bc5d60a231dab
singhabhishek22957 <singhabhishek22957@gmai.com>
2025-05-27 22:44:04 +0530

    404.html changes 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -29,7 +29,7 @@ createRoot(document.getElementById("root")).render(
     </AppProvider> */}
     <UserProvider>
       <CharityProvider>
-        <RouterProvider router={router} />
+        <RouterProvider router={router} basename="/totheppoint_login" />
       </CharityProvider>
     </UserProvider>
   </StrictMode>
